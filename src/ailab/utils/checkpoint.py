437d9b203ecbcac578eb5399d1a6b8dc89c2cb04
wubo <haohuawu1999@163.com>
2025-04-29 01:22:11 +0000

    feat(checkpoint):添加去除框架保存的checkpoint中所有键都被包裹一层module.的情况

--- src/ailab/utils/checkpoint.py
+++ src/ailab/utils/checkpoint.py
@@ -19,3 +19,20 @@ class Checkpointer:
         model.load_state_dict(ckpt['model_state'])
         optimizer.load_state_dict(ckpt['optimizer_state'])
         return ckpt['epoch']
+
+
+def remove_module_prefix(state_dict):
+    """
+    去掉state_dict中所有key的'module.'前缀。
+    
+    :param state_dict: 包含模型参数和其值的有序字典。
+    :return: 新的有序字典，其中所有的key都没有'module.'前缀。
+    """
+    new_state_dict = {}
+    for key, value in state_dict.items():
+        if key.startswith('module.'):
+            new_key = key[len('module.'):]  # 去掉'module.'前缀
+        else:
+            new_key = key
+        new_state_dict[new_key] = value
+    return new_state_dict
